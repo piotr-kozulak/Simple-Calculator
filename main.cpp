7650ee6c1d16b90c5362985c441874585f8fdeab
Piotrek <252760@edu.p.lodz.pl>
2024-05-12 14:01:27 +0200

    added simple menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cstring>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void usage(char *programName) {
+    printf("Usage: %s number1 operation number\n", programName);
+    printf("Available operations:\n");
+    printf("\tadd\n");
+    printf("\tsub\n");
+    printf("\tmul\n");
+    printf("\tdiv\n");
+    printf("\n");
+}
+int main (int argc, char *argv[]){
+
+
+usage (argv[0]);
     return 0;
 }
